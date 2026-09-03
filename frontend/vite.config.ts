e4c58ce01e080c74bcf0a031e9015bde4285b542
Gabriel Cunha <gabrielantcunha@gmail.com>
2026-09-02 22:11:54 -0300

    chore(frontend): adiciona ferramentas de composição da interface

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), reactClickToComponent()],
   server: {
     proxy: {
       '/api': {
